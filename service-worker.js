3ef6ff10d447754b0ec7a824d7389d212bb5f5df
BMS-TOOL <richard.bravo@se.com>
2026-09-25 16:41:32 -0400

    Update cache name to version 8.3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // BMS Troubleshooter service worker - application version 8.2
-const CACHE_NAME = 'bms-troubleshooter-v8-2';
+const CACHE_NAME = 'bms-troubleshooter-v8-3';
 const CORE_FILES = [
   './',
   './index.html',
